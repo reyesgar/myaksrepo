6253431d3cbdc40ef80c1fe527eb8ee81adf2d05
reyesgar <66829791+reyesgar@users.noreply.github.com>
2025-04-21 14:34:23 -0400

    new k8 try

--- main.tf
+++ main.tf
@@ -1,33 +1,21 @@
-#main.tf
-
-data "http" "aksc_release" {
-  url = "https://github.com/Azure/AKS-Construction/releases/download/0.10.7/main.json"
-  request_headers = {
-    Accept = "application/json"
-    User-Agent = "request module"
-  }
-}
-
-data "azurerm_client_config" "current" {}
-
 resource "azurerm_resource_group" "rg" {
-  name = var.resourceGroupName
-  location = var.location
+  name     = "learnk8sResourceGroup"
+  location = "eastus"
 }
 
-resource "azurerm_resource_group_template_deployment" "aksc_deploy" {
-  name = "AKS-C"
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "learnk8scluster"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  deployment_mode = "Incremental"
-  template_content = data.http.aksc_release.response_body
-  parameters_content = jsonencode({
-    resourceName = {value=var.resourceName}
-    agentCount = {value=var.agentCount}
-    JustUseSystemPool = {value=var.JustUseSystemPool}
-    osDiskType = {value=var.osDiskType}
-    osDiskSizeGB = {value=var.osDiskSizeGB}
-    registries_sku = {value=var.registries_sku}
-    acrPushRolePrincipalId = {value=data.azurerm_client_config.current.object_id}
-    automationAccountScheduledStartStop = {value=var.automationAccountScheduledStartStop}
-  })
+  dns_prefix          = "learnk8scluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = "2"
+    vm_size    = "standard_d2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,5 @@
-#outputs.tf
-
-output aksClusterName {
-  value = jsondecode(azurerm_resource_group_template_deployment.aksc_deploy.output_content).aksClusterName.value
-  description = "The name of the AKS cluster."
-}
-output userClientId {
-  value = data.azurerm_client_config.current.client_id
-  description = "Current User ClientId"
-}
-output userObjectId {
-  value = data.azurerm_client_config.current.object_id
-  description = "Current User ObjectId"
+resource "local_file" "kubeconfig" {
+  depends_on   = [azurerm_kubernetes_cluster.cluster]
+  filename     = "kubeconfig"
+  content      = azurerm_kubernetes_cluster.cluster.kube_config_raw
 }
