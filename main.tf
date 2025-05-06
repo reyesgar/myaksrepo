b86826b1c42a05964f3bbecd30f150ae69459b5c
reyesgar <66829791+reyesgar@users.noreply.github.com>
2025-05-06 13:06:12 -0400

    try new aks code

--- main.tf
+++ main.tf
@@ -1,26 +1,45 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "learnk8sResourceGroup"
-  location = "northeurope"
+# Generate random resource group name
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
-resource "azurerm_kubernetes_cluster" "cluster" {
-  name                = "learnk8scluster"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "learnk8scluster"
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
 
-  default_node_pool {
-    name       = "default"
-    node_count = "2"
-    vm_size    = "standard_d2_v2"
-  }
+resource "random_pet" "azurerm_kubernetes_cluster_name" {
+  prefix = "cluster"
+}
+
+resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
+  prefix = "dns"
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  location            = azurerm_resource_group.rg.location
+  name                = random_pet.azurerm_kubernetes_cluster_name.id
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
+
   identity {
     type = "SystemAssigned"
   }
-  #addon_profile {
-  #  http_application_routing {
-  #    enabled = true
-  #  }
-  #}
-  
+
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D2_v2"
+    node_count = var.node_count
+  }
+  linux_profile {
+    admin_username = var.username
+
+    ssh_key {
+      key_data = azapi_resource_action.ssh_public_key_gen.output.publicKey
+    }
+  }
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
 }
