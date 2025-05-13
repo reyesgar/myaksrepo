22c0863d69044b13e8e900ee0bafb35b325b50bb
reyesgar <66829791+reyesgar@users.noreply.github.com>
2025-05-13 12:38:26 -0400

    remove host encryption

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     name       = "agentpool"
     vm_size    = "Standard_D2_v2"
     node_count = var.node_count
-    host_encryption_enabled = true
+    #host_encryption_enabled = true
   }
   linux_profile {
     admin_username = var.username
