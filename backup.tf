de17d67e73eed3e49601f59f7e40fb25b7c355a6
reyesgar <66829791+reyesgar@users.noreply.github.com>
2025-05-14 15:22:59 -0400

    add depends on for role assignments

--- backup.tf
+++ backup.tf
@@ -97,42 +97,49 @@ resource "azurerm_role_assignment" "test_extension_and_storage_account_permissio
   scope                = azurerm_storage_account.example.id
   role_definition_name = "Storage Account Contributor"
   principal_id         = azurerm_kubernetes_cluster_extension.example.aks_assigned_identity[0].principal_id
+  depends_on = [ azurerm_resource_group.rg,azurerm_kubernetes_cluster.k8s ]
 }
 
 resource "azurerm_role_assignment" "test_vault_msi_read_on_cluster" {
   scope                = data.azurerm_kubernetes_cluster.k8s.id
   role_definition_name = "Reader"
   principal_id         = azurerm_data_protection_backup_vault.example.identity[0].principal_id
+  depends_on = [ azurerm_resource_group.rg,azurerm_kubernetes_cluster.k8s ]
 }
 
 resource "azurerm_role_assignment" "test_vault_msi_read_on_snap_rg" {
   scope                = data.azurerm_resource_group.import.id
   role_definition_name = "Reader"
   principal_id         = azurerm_data_protection_backup_vault.example.identity[0].principal_id
+  depends_on = [ azurerm_resource_group.rg,azurerm_kubernetes_cluster.k8s ]
 }
 
 resource "azurerm_role_assignment" "test_vault_msi_snapshot_contributor_on_snap_rg" {
   scope                = data.azurerm_resource_group.import.id
   role_definition_name = "Disk Snapshot Contributor"
   principal_id         = azurerm_data_protection_backup_vault.example.identity[0].principal_id
+  depends_on = [ azurerm_resource_group.rg,azurerm_kubernetes_cluster.k8s ]
 }
 
 resource "azurerm_role_assignment" "test_vault_data_operator_on_snap_rg" {
   scope                = data.azurerm_resource_group.import.id
   role_definition_name = "Data Operator for Managed Disks"
   principal_id         = azurerm_data_protection_backup_vault.example.identity[0].principal_id
+  depends_on = [ azurerm_resource_group.rg,azurerm_kubernetes_cluster.k8s ]
 }
 
 resource "azurerm_role_assignment" "test_vault_data_contributor_on_storage" {
   scope                = azurerm_storage_account.example.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_data_protection_backup_vault.example.identity[0].principal_id
+  depends_on = [ azurerm_resource_group.rg,azurerm_kubernetes_cluster.k8s ]
 }
 
 resource "azurerm_role_assignment" "test_cluster_msi_contributor_on_snap_rg" {
   scope                = data.azurerm_resource_group.import.id
   role_definition_name = "Contributor"
   principal_id         = data.azurerm_kubernetes_cluster.k8s.identity[0].principal_id
+  depends_on = [ azurerm_resource_group.rg,azurerm_kubernetes_cluster.k8s ]
 }
 
 resource "azurerm_data_protection_backup_policy_kubernetes_cluster" "example" {
@@ -186,6 +193,7 @@ resource "azurerm_data_protection_backup_instance_kubernetes_cluster" "example" 
   }
 
   depends_on = [
+    azurerm_data_protection_backup_vault.example,
     azurerm_role_assignment.test_extension_and_storage_account_permission,
     azurerm_role_assignment.test_vault_msi_read_on_cluster,
     azurerm_role_assignment.test_vault_msi_read_on_snap_rg,
